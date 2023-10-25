c800f8cb09e977d676ae8953eb68131baabfef25
Mostafa <mostafafawzy7107@gmail.com>
2023-10-25 14:38:50 +0300

    SLNT-1 | Airbnb Config - Add airbnb typescript config

---
+++ index.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "airbnb-typescript/base",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+  ],
+  plugins: ["react", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.eslint.json",
+  },
+  rules: {
+    "import/no-extraneous-dependencies": "off",
+    "no-undef": "off",
+    "import/extensions": "off",
+  },
+};
